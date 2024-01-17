ef351f0bccdf9836b26a960370750bb2c0114f5b
“Katie-W-22” <katiecoder22@gmail.com>
2024-01-17 17:04:10 +0000

    moved responsive div

--- katie2-app/app/comps/Large.tsx
+++ katie2-app/app/comps/Large.tsx
@@ -2,7 +2,7 @@
 
 export default function Large() {
     return (
-        <div className='large__screen hidden md:block'>
+        <div className='items-center justify-center h-full hidden md:block'>
             <div className='large__screen__text'>
                 <p className='large__screen__text__title'>Katie Williams</p>
                 <p className='large__screen__text__subtitle'>Full Stack Developer</p>
