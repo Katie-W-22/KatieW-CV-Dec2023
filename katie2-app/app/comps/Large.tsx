106bea1114f3eda0435e0ec670136929f2791db9
“Katie-W-22” <katiecoder22@gmail.com>
2024-01-17 17:07:21 +0000

    responsive styling edit

--- katie2-app/app/comps/Large.tsx
+++ katie2-app/app/comps/Large.tsx
@@ -2,7 +2,7 @@
 
 export default function Large() {
     return (
-        <div className='items-center justify-center h-full hidden md:block'>
+        <div className='"fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 hidden md:block'>
             <div className='large__screen__text'>
                 <p className='large__screen__text__title'>Katie Williams</p>
                 <p className='large__screen__text__subtitle'>Full Stack Developer</p>
